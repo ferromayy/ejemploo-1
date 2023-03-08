1308ed3ebe46d860c8dc6f806ec420d650598c39
ferromayy <ferromay.is@icloud.com>
2023-03-08 13:16:01 -0300

    commit1rama2

--- .js
+++ .js
@@ -1,6 +1,7 @@
 const autito = "autitos";
 let autitos2 = ["uno", "dos", "tres"];
 let autitoPeligrosooo = [1, 1, 2];
+let autitoPolicia = 2;
 
 const peleaAutitos = (autitoPeligrosooo) => {
   let autitoLadron = [];
