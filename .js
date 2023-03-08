386899e726021921721abf13b3802bb25c9d0d90
ferromayy <ferromay.is@icloud.com>
2023-03-08 12:51:42 -0300

    segundo commit

--- .js
+++ .js
@@ -1,2 +1,8 @@
-const autito = autito 
+const autito = "autitos";
 let autito2 = [];
+let autitoPeligrosooo = [1, 1, 2];
+const peleaAutitos = (autitoPeligrosooo) => {
+  let autitoLadron = [];
+  autitoLadron.push(autitoPeligrosooo);
+  console.log(autitoLadron);
+};
